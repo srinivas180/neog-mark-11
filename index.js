376fbd7c66e24164bab672592a7a7702214b21d4
Srinivas <srinivas365@proton.me>
2022-10-03 15:23:28 +0530

    Added Logic

---
+++ index.js
@@ -0,0 +1,31 @@
+var dobHTML = document.querySelector("#dob");
+var luckyNumberHTML = document.querySelector("#lucky-num");
+var outputDiv = document.querySelector("#output");
+
+var btnCheckLuckyNum = document.querySelector("#check-lucky-num");
+
+function isYourBirthdayLucky() {
+    var dobValue = dobHTML.value;
+    var luckyNum = luckyNumberHTML.value;
+    var sum = 0;
+
+    if(dobValue == "" || luckyNum == "") {
+        outputDiv.innerText = "Enter both fields to check whether your birthday is lucky or not."
+    }
+    else {
+        dobValue = dobValue.replaceAll("-", "");
+        for(var i = 0; i < dobValue.length; i++) {
+            sum += parseInt(dobValue.charAt(i));
+        }
+        if(sum % luckyNum == 0) {
+            //lucky
+            outputDiv.innerText = "Your birthday is lucky";
+        }
+        else {
+            //unlucky
+            outputDiv.innerText = "Your birthday is not lucky";
+        }
+    }
+}
+
+btnCheckLuckyNum.addEventListener("click", isYourBirthdayLucky);
